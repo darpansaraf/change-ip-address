b3e87f59073759468912de3d35b68f284cbdd1ef
dsaraf@tavisca.com <dsaraf@tavisca.com>
2016-01-24 22:27:13 +0530

    Added Python script which reverts the IP address of wireless adapter.

---
+++ RevertTheIpAddress.py
@@ -0,0 +1,12 @@
+__author__ = 'Darpan'
+
+import wmi
+
+# Obtain network adaptors configurations
+nic_configs = wmi.WMI().Win32_NetworkAdapterConfiguration(IPEnabled=True)
+
+# First network adaptor
+nic = nic_configs[0]
+
+# Enable DHCP
+nic.EnableDHCP()
